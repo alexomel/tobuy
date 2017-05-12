b984a0ce3bfdf52e6f16a228af5229daff791b69
Nikita <mykytanivikov@gmail.com>
2017-05-12 16:38:53 +0200

    Server init

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+
+});
