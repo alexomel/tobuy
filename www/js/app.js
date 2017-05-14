639db12310e99dc7d6451971e91df04b448b48e6
Administrator <sasha.omely@gmail.com>
2017-05-14 19:19:42 +0200

    changes

---
+++ www/js/app.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAY1t4H9D3PUeu4j6_odxV1xQ5MAhS_S9Q",
+    authDomain: "tobuy-68833.firebaseapp.com",
+    databaseURL: "https://tobuy-68833.firebaseio.com",
+    projectId: "tobuy-68833",
+    storageBucket: "tobuy-68833.appspot.com",
+    messagingSenderId: "1092640222732"
+  };
+  firebase.initializeApp(config);
+</script>
